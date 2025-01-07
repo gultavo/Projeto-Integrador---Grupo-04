64231f1b444dfc4df648192a3504e7e0c4133b98
EmanuelBSM <emanuelbsmartins@gmail.com>
2025-01-07 20:30:27 -0300

    Imagens são traduzidas

---
+++ JavaScript/mudarIMG.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() { 
+
+    function traduzimg(){
+        let imagem1 = document.getElementById('img1');
+        let imagem2 = document.getElementById('img2');
+        let imagem3 = document.getElementById('img3');
+        let imagem4 = document.getElementById('img4');
+
+        if(segundo.state == true){
+            imagem1.src = 'Imagens/CarrosselT1.png';
+            imagem2.src = 'Imagens/CarrosselT2.png';
+            imagem3.src = 'Imagens/CarrosselT3.png';
+            imagem4.src = 'Imagens/CarrosselT4.png';
+        }
+        else if(segundo.state == false){
+            imagem1.src = 'Imagens/carrossel.png';
+            imagem2.src = 'Imagens/carrossel2.jpg';
+            imagem3.src = 'Imagens/carrossel3.jpg';
+            imagem4.src = 'Imagens/carrossel4.jpg';
+    }
+    }   
+
+    function mudaEstado(botao){
+        botao.state = !botao.state;
+    }
+
+
+
+    const segundo = document.getElementById('segundo');
+    segundo.state = false;
+    segundo.addEventListener('click', function(){
+        traduzimg();
+        mudaEstado(segundo);
+    });
+
+});

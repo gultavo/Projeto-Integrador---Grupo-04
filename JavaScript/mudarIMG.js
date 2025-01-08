f9da3cc18a7ead61e3c00b926580410acf252a33
gultavo <gusborsatto@gmail.com>
2025-01-08 20:40:44 -0300

    Algumas atualizações

--- JavaScript/mudarIMG.js
+++ JavaScript/mudarIMG.js
@@ -1,5 +1,5 @@
-const vars = ["ti1", "t1", "ti2", "t2", "ti3", "t3", "ti4", "t4", "m1", "ti5", "t5", "ti6", 
-"t6", "ti7", "t7", "ti8", "t8", "ti9", "t9", "ti10", "ti11", "ti12", "a1", "a2", "a3", "a4", "m2", "m3"]
+const vars = ["a1", "a2", "a3", "a4", "ti1", "t1", "ti2", "t2", "m1", "ti3", "t3", "ti4", "t4", "m2", "ti5", "t5", "ti6",
+    "t6", "ti7", "t7", "m3", "ti8", "t8", "ti9", "t9", "ti10", "ti11", "ti12"]
 
 variaveis = []
 
@@ -7,6 +7,7 @@ for (let i = 0; i < vars.length; i++) {
     const element = document.getElementById(vars[i])
     variaveis.push(element);
 }
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -24,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
             imagem4.src = 'Imagens/carrosselt4.png';
         }
         else {
-            imagem1.src = 'Imagens/carrossel1.png';
+            imagem1.src = 'Imagens/carrossel.png';
             imagem2.src = 'Imagens/carrossel2.png';
             imagem3.src = 'Imagens/carrossel3.png';
             imagem4.src = 'Imagens/carrossel4.png';
@@ -48,21 +49,34 @@ main()
 
 async function main() {
     const dataJson = await getData();
+
     for (let i = 0; i < dataJson.length; i++) {
         // const key = Object.keys(dataJson[i]);
-        for (let i = 0; )
-        variaveis[0].textContent = dataJson[0]["ti1"];
-        variaveis[1].textContent = dataJson[0]["t1"];
-        variaveis[2].textContent = dataJson[1]["ti2"];
-        variaveis[3].textContent = dataJson[1]["t2"];
-        variaveis[4].textContent = dataJson[2]["ti3"];
-        variaveis[5].textContent = dataJson[2]["t3"];
-        variaveis[6].textContent = dataJson[3]["ti4"];
-        variaveis[7].textContent = dataJson[3]["t4"];
-        variaveis[8].textContent = dataJson[4]["m1"];
-        variaveis[9].textContent = dataJson[4]["t5"];
     }
-    console.log(dataJson[4]["t5"])
+
+    // variaveis[0].textContent = dataJson[0]["a1"];
+    // variaveis[1].textContent = dataJson[0]["a2"];
+    // variaveis[2].textContent = dataJson[1]["a3"];
+    // variaveis[3].textContent = dataJson[1]["a4"];
+    // variaveis[4].textContent = dataJson[2]["ti1"];
+    // variaveis[5].textContent = dataJson[2]["t1"];
+    // variaveis[6].textContent = dataJson[3]["ti2"];
+    // variaveis[7].textContent = dataJson[3]["t2"];
+    // variaveis[8].textContent = dataJson[4]["ti3"];
+    // variaveis[9].textContent = dataJson[4]["t3"];
+
+
+    cont = -1;
+
+    for (let i = 0; i <= 13; i++) {
+        for (let h = 0; h <= 1; h++) {
+            cont += 1
+            console.log(`variaveis[${cont}].textContent = dataJson[${i}][${vars[cont]}]`);
+            // variaveis[cont].textContent = "aaaaaaaaaaaaaaaaaa";
+            variaveis[cont].textContent = dataJson[i][vars[cont]];
+        }
+    }
+
 }
 
 async function getData() {

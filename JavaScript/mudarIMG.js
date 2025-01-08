8e7fb7e6d3c456715d1d95c644709a015e889c20
gultavo <gusborsatto@gmail.com>
2025-01-08 00:00:13 -0300

    Parte 2, JSON, conversor de textos

--- JavaScript/mudarIMG.js
+++ JavaScript/mudarIMG.js
@@ -1,4 +1,15 @@
-const vars = ["ti1", "t1", "ti2", "t2", "ti3", "t3", "ti4", ""]
+const vars = ["ti1", "t1", "ti2", "t2", "m1", "ti3", "t3", "ti4", "t4", "m2", "t5", "ti5", "t5", "ti6", 
+"t6", "ti7", "t7", "m3", "ti8", "t8", "ti9", "t9", "ti10", "t10", "ti11", "t11", "ti12", "t12"]
+
+variaveis = []
+let t1 = document.getElementById("t1");
+
+for (let i = 0; i < vars.length; i++) {
+    const element = document.getElementById(vars[i])
+    variaveis.push(element);
+}
+
+console.log(variaveis[1])
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -40,22 +51,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-
-
-
-
-
-
-
-
-
 main()
 
 async function main() {
     const dataJson = await getData();
     for (let i = 0; i < dataJson.length; i++) {
-        const text = Object(dataJson[i]);
-        console.log(text);
+        // const key = Object.keys(dataJson[i]);
+        variaveis[0].textContent = dataJson[0]["ti1"];
+        variaveis[1].textContent = dataJson[0]["t1"];
+        variaveis[2].textContent = dataJson[1]["ti2"];
+        variaveis[3].textContent = dataJson[1]["t2"];
+        variaveis[5].textContent = dataJson[2]["ti3"];
+        variaveis[6].textContent = dataJson[2]["t3"];
     }
 }
 
@@ -63,11 +70,4 @@ async function getData() {
     const res = await fetch("./json/texts.json");
     const data = await res.json();
     return data;
-}
-
-console.log("atualizado");
-
-
-function trocarTexto() {
-
 }

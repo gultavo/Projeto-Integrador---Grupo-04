7b70f4d88d1f2b26a424f4b2177b33edaf5e5ec0
gultavo <gusborsatto@gmail.com>
2025-01-08 14:47:29 -0300

    Algumas coisas a mais

--- JavaScript/mudarIMG.js
+++ JavaScript/mudarIMG.js
@@ -1,6 +1,5 @@
-const vars = ["ti1", "t1", "ti2", "t2", "ti3", "t3", "ti4", "t4", "t5", "ti5", "t5", "ti6", 
-"t6", "ti7", "t7", "ti8", "t8", "ti9", "t9", "ti10", "ti11", "ti12", "a1", "a2", "a3", "a4", 
-"m1", "m2", "m3"]
+const vars = ["ti1", "t1", "ti2", "t2", "ti3", "t3", "ti4", "t4", "m1", "ti5", "t5", "ti6", 
+"t6", "ti7", "t7", "ti8", "t8", "ti9", "t9", "ti10", "ti11", "ti12", "a1", "a2", "a3", "a4", "m2", "m3"]
 
 variaveis = []
 
@@ -9,27 +8,22 @@ for (let i = 0; i < vars.length; i++) {
     variaveis.push(element);
 }
 
-console.log(variaveis[1])
-
 
 document.addEventListener('DOMContentLoaded', function () {
 
     function traduzimg() {
-        console.log("trad");
         let imagem1 = document.getElementById('img1');
         let imagem2 = document.getElementById('img2');
         let imagem3 = document.getElementById('img3');
         let imagem4 = document.getElementById('img4');
 
         if (segundo.state == true) {
-            console.log("vdd");
             imagem1.src = 'Imagens/carrosselt1.png';
             imagem2.src = 'Imagens/carrosselt2.png';
             imagem3.src = 'Imagens/carrosselt3.png';
             imagem4.src = 'Imagens/carrosselt4.png';
         }
         else {
-            console.log("falso");
             imagem1.src = 'Imagens/carrossel1.png';
             imagem2.src = 'Imagens/carrossel2.png';
             imagem3.src = 'Imagens/carrossel3.png';
@@ -38,7 +32,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function mudaEstado(botao) {
-        console.log("mudou");
         botao.state = !botao.state;
     }
 
@@ -57,7 +50,7 @@ async function main() {
     const dataJson = await getData();
     for (let i = 0; i < dataJson.length; i++) {
         // const key = Object.keys(dataJson[i]);
-        console.log(dataJson[0]["ti1"]);
+        for (let i = 0; )
         variaveis[0].textContent = dataJson[0]["ti1"];
         variaveis[1].textContent = dataJson[0]["t1"];
         variaveis[2].textContent = dataJson[1]["ti2"];
@@ -66,10 +59,10 @@ async function main() {
         variaveis[5].textContent = dataJson[2]["t3"];
         variaveis[6].textContent = dataJson[3]["ti4"];
         variaveis[7].textContent = dataJson[3]["t4"];
-        variaveis[8].textContent = dataJson[4]["ti5"];
-        variaveis[9].textContent = dataJson[5]["t5"];
-        variaveis[10].textContent = dataJson
+        variaveis[8].textContent = dataJson[4]["m1"];
+        variaveis[9].textContent = dataJson[4]["t5"];
     }
+    console.log(dataJson[4]["t5"])
 }
 
 async function getData() {

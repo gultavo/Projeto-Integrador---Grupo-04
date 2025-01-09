bb4250c367e7a73917b60bd5f04ef95e96feb21d
gultavo <gusborsatto@gmail.com>
2025-01-08 21:04:11 -0300

    Textos prontos no JSON

--- JavaScript/mudarIMG.js
+++ JavaScript/mudarIMG.js
@@ -1,5 +1,5 @@
-const vars = ["a1", "a2", "a3", "a4", "ti1", "t1", "ti2", "t2", "m1", "ti3", "t3", "ti4", "t4", "m2", "ti5", "t5", "ti6",
-    "t6", "ti7", "t7", "m3", "ti8", "t8", "ti9", "t9", "ti10", "ti11", "ti12"]
+const vars = ["a1", "a2", "a3", "a4", "ti1", "t1", "ti2", "t2", "ti3", "t3", "ti4", "t4", "m1",
+"m2", "ti5", "t5", "ti6","t6", "ti7", "t7", "ti8", "t8", "ti9", "t9", "ti10", "ti11", "ti12", "m3"]
 
 variaveis = []
 
@@ -49,30 +49,12 @@ main()
 
 async function main() {
     const dataJson = await getData();
-
-    for (let i = 0; i < dataJson.length; i++) {
-        // const key = Object.keys(dataJson[i]);
-    }
-
-    // variaveis[0].textContent = dataJson[0]["a1"];
-    // variaveis[1].textContent = dataJson[0]["a2"];
-    // variaveis[2].textContent = dataJson[1]["a3"];
-    // variaveis[3].textContent = dataJson[1]["a4"];
-    // variaveis[4].textContent = dataJson[2]["ti1"];
-    // variaveis[5].textContent = dataJson[2]["t1"];
-    // variaveis[6].textContent = dataJson[3]["ti2"];
-    // variaveis[7].textContent = dataJson[3]["t2"];
-    // variaveis[8].textContent = dataJson[4]["ti3"];
-    // variaveis[9].textContent = dataJson[4]["t3"];
-
-
     cont = -1;
 
     for (let i = 0; i <= 13; i++) {
         for (let h = 0; h <= 1; h++) {
             cont += 1
             console.log(`variaveis[${cont}].textContent = dataJson[${i}][${vars[cont]}]`);
-            // variaveis[cont].textContent = "aaaaaaaaaaaaaaaaaa";
             variaveis[cont].textContent = dataJson[i][vars[cont]];
         }
     }

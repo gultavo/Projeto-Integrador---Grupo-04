ecc50a3e213dab0a21f000357b9cf36d676b447d
EmanuelBSM <emanuelbsmartins@gmail.com>
2025-01-08 22:45:21 -0300

    audiodescrição pronta

--- JavaScript/audiodescricao.js
+++ JavaScript/audiodescricao.js
@@ -1,16 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     function readContent() {
-        for (let i = 0; i < 27; i++) {
-            var text = document.getElementsByClassName('text')[i].innerText;
-            if (segundo.state == false) {
-            var voice = 'Brazilian Portuguese Female';
-            responsiveVoice.speak(text, voice);
-        }
-        else if (segundo.state == true) {
-            var voice = 'US English Male';
-            responsiveVoice.speak(text, voice);
-        }
+        for (let i = 0; i < 100; i++) {
+            var text = document.getElementsByClassName('text')[i];
+            if (text.tagName.toLowerCase() === 'img'){
+                if (segundo.state == false) {
+                    var alt = text.alt;
+                    var voice = 'Brazilian Portuguese Female';
+                    responsiveVoice.speak(alt, voice);
+                }
+                else if (segundo.state == true) {
+                    var alt = text.alt;
+                    var voice = 'US English Male';
+                    responsiveVoice.speak(alt, voice);
+                }
+            }
+            else{
+                if (segundo.state == false) {
+                    var text = text.innerHTML;
+                    var voice = 'Brazilian Portuguese Female';
+                    responsiveVoice.speak(text, voice);
+                }
+                else if (segundo.state == true) {
+                    var text = text.innerHTML;
+                    var voice = 'US English Male';
+                    responsiveVoice.speak(text, voice);
+                }
+            }
+
     }};
 
     const primeiro = document.getElementById('primeiro');
